6c0a12467c8381f10cf66872a061f4ac451a66b7
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2023-01-05 12:23:06 +0200

    implemente button publier

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,13 @@ function App() {
         <Search />
         <Main />
       </div>
+      <button
+        class="btn h-12 uppercase inline-block px-6 py-2.5 bg-black text-white hover:text-black focus:text-black font-bold text-xs leading-tight uppercase rounded shadow-md hover:bg-white hover:shadow-lg focus:bg-white  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-white active:shadow-lg transition duration-150 ease-in-out flex items-center"
+        type="button"
+        id="button-addon2"
+      >
+        publier
+      </button>
     </div>
   );
 }

f18b5fb3e54e506a3ef2c110cbe6a2417145a0a9
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2023-01-05 11:39:10 +0200

    implemente style main

---
+++ client/src/layout/main/Main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <main className="w-[70%] my-auto h-[80vh] bg-white backdrop-filter backdrop-blur-lg bg-opacity-10"></main>
+  );
+}

fea0ac4dab147f003a79dc81abcfb67a89eea309
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2023-01-05 11:56:42 +0200

    implementesearchbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 
 import "./App.css";
 import Main from "./layout/main/Main";
+import Search from "./layout/serchbar/Search";
 import Sidebar from "./layout/sidebar/Sidebar";
 
 function App() {
   return (
     <div className="flex gap-12">
       <Sidebar />
-      <Main />
+      <div className="flex flex-col w-[70%] my-auto">
+        <Search />
+        <Main />
+      </div>
     </div>
   );
 }
--- client/src/layout/main/Main.jsx
+++ client/src/layout/main/Main.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Main() {
   return (
-    <main className="w-[70%] my-auto h-[80vh] bg-white backdrop-filter backdrop-blur-lg bg-opacity-10"></main>
+    <main className="w-[100%] my-auto h-[80vh] bg-white backdrop-filter backdrop-blur-lg bg-opacity-10"></main>
   );
 }
